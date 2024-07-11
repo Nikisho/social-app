4a559c6b0553c6b9e312dac9b2ff629c4c6ca913
Nicholas <phobereromain@gmail.com>
2024-07-11 16:47:11 +0100

    added stack nav

--- App.tsx
+++ App.tsx
@@ -1,16 +1,36 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Navbar from './src/components/Navbar';
 import HomeScreen from './src/screens/home/HomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createStackNavigator } from '@react-navigation/stack';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import ProfileScreen from './src/screens/profile/ProfileScreen';
 
+
+const Stack = createStackNavigator();
+const mainTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#FFF59D'
+  },
+};
 export default function App() {
   return (
     <SafeAreaProvider className="">
       <SafeAreaView className='flex h-full bg-yellow-200'>
-        <HomeScreen />
-        <Navbar />
+        <NavigationContainer theme={mainTheme} >
+          <Stack.Navigator screenOptions={{
+            headerShown: false
+          }} >
+            <Stack.Screen name="home" component={HomeScreen} />
+            <Stack.Screen name="profile" component={ProfileScreen} />
+          </Stack.Navigator>
+          <Navbar />
+        </NavigationContainer>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,30 +3,37 @@ import React from 'react'
 import { Entypo } from '@expo/vector-icons';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from 'react-native-screens/lib/typescript/native-stack/types';
+
 
 const Navbar = () => {
   const menuItems = [
     {
-      icon: <AntDesign name="search1" size={30} color="black" />
+      icon: <AntDesign name="search1" size={30} color="black" />,
+      navigation:'search'
     },
     {
-      icon: <Entypo name="home" size={30} color="black" />
-
+      icon: <Entypo name="home" size={30} color="black" />,
+      navigation:'home'
     },
     {
-      icon: <Entypo name="message" size={30} color="black" />
+      icon: <Entypo name="message" size={30} color="black" />,
+      navigation:'messages'
     },
     {
-      icon: <Ionicons name="person" size={30} color="black" />
+      icon: <Ionicons name="person" size={30} color="black" />,
+      navigation:'profile'
     }
   ];
-
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
   return (
     <View className=' absolute bg-amber-300 inset-x-0 bottom-0 h-16 flex justify-between flex-row'>
 
       {
         menuItems.map((item) => (
-          <TouchableOpacity key={menuItems.indexOf(item)} className={` flex justify-center w-1/4 items-center`}>
+          <TouchableOpacity key={menuItems.indexOf(item)} className={` flex justify-center w-1/4 items-center`} 
+              onPress={() => navigation.navigate(item.navigation)}>
             <Text className='text-lg'>{item.icon}</Text>
           </TouchableOpacity>
         ))
