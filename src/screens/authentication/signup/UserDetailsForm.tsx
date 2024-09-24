940d05f26db8db76922035005f9ef06ab757e7ef
Romain Phobere <romainphobere@Romains-MacBook-Air.local>
2024-09-24 23:34:07 +0100

    changed p- to py-4, py-2 in userdetailsfrom

--- src/screens/authentication/signup/UserDetailsForm.tsx
+++ src/screens/authentication/signup/UserDetailsForm.tsx
@@ -33,7 +33,7 @@ const UserDetailsForm: React.FC<UserDetailsFormProps> = ({
                 </Text>
                 <TextInput
                     placeholder='Enter username '
-                    className={` px-5 flex items-center rounded-full border bg-gray-200 ${Platform.OS === 'ios'? 'p-4' : 'p-2'}`}
+                    className={` px-5 flex items-center rounded-full border bg-gray-200 ${Platform.OS === 'ios'? 'py-4' : 'py-2'}`}
                     value={name}
                     maxLength={15}
                     onChangeText={(value) => updateFields({ name: value.replace(/[^a-z0-9]/gi, '') })}
@@ -49,7 +49,7 @@ const UserDetailsForm: React.FC<UserDetailsFormProps> = ({
                     placeholder='Enter age '
                     value={age}
                     maxLength={2}
-                    className={`px-5 flex items-center border w-1/3 rounded-full bg-gray-200 ${Platform.OS === 'ios' ? 'p-4' : 'p-2'}`}
+                    className={`px-5 flex items-center border w-1/3 rounded-full bg-gray-200 ${Platform.OS === 'ios' ? 'py-4' : 'py-2'}`}
                     onChangeText={(value) => updateFields({ age: value.replace(/[^0-9]/g, '') })}
                 />
 
