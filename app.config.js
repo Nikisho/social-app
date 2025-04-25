b192934088b6437800bcdcca2abd1410cd6785d5
Romain Phobere <phobereromain@gmail.com>
2025-04-25 18:37:21 +0100

    changed version number

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default {
     ],
     name: "Linkzy",
     slug: "social-app",
-    version: "1.2.4",
+    version: "1.2.5",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -44,14 +44,14 @@ export default {
       bundleIdentifier: 'com.linkzy',
       usesAppleSignIn: true,
       googleServicesFile: "./GoogleService-Info.plist",
-      buildNumber: '1.1.4'
+      buildNumber: '1.1.5'
     },
     android: {
       googleServicesFile: './google-services.json', // Set to path where file will be written
       // ndkVersion: "26.1.10909125",
       package: "com.linkzy",
       useNextNotificationsApi: true,
-      versionCode: 17,
+      versionCode: 18,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
