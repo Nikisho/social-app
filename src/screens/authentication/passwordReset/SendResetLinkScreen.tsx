f5eaf8d70838f879bd4acf86e1c1a2bc3ccc7498
Romain Phobere <phobereromain@gmail.com>
2025-10-16 21:34:42 +0100

    reset pw typo fixed

--- src/screens/authentication/passwordReset/SendResetLinkScreen.tsx
+++ src/screens/authentication/passwordReset/SendResetLinkScreen.tsx
@@ -9,6 +9,7 @@ import { useNavigation } from '@react-navigation/native';
 import { RootStackNavigationProp } from '../../../utils/types/types';
 import { handleUrl } from '../../../utils/functions/handleUrl';
 import platformAlert from '../../../utils/functions/platformAlert';
+import SecondaryHeader from '../../../components/SecondaryHeader';
 
 const SendResetLinkScreen = () => {
     const navigation = useNavigation<RootStackNavigationProp>();
@@ -24,16 +25,16 @@ const SendResetLinkScreen = () => {
             return;
         }
         const { error } = await supabase.auth.resetPasswordForEmail(email, {
-            redirectTo: 'com.linkzy://resetpassword', 
+            redirectTo: 'com.linkzy://resetpassword',
         });
-        if (error)  console.error(error.message);
+        if (error) console.error(error.message);
         platformAlert("We've sent you a link to reset your password.")
     };
- 
+
     useEffect(() => {
         const subscription = Linking.addEventListener('url', ({ url }) => handleUrl(url, navigation));
         Linking.getInitialURL().then((url) => {
-            if (url) {handleUrl(url, navigation);}
+            if (url) { handleUrl(url, navigation); }
         });
         return () => {
             subscription.remove();
@@ -41,36 +42,39 @@ const SendResetLinkScreen = () => {
     }, []);
 
     return (
-        <View className='flex h-1/2 justify-center px-5'>
-            <TouchableOpacity className="py-5 " onPress={() => navigation.goBack()}>
+        <>
+            <SecondaryHeader displayText='Reset your password' />
+            <View className='flex h-1/2 justify-center px-5'>
+                {/* <TouchableOpacity className="py-5 " onPress={() => navigation.goBack()}>
                 <Ionicons name="chevron-back-circle-outline" size={30} color="black" />
-            </TouchableOpacity>
-            <View className='w-full flex items-center space-y-5'>
-                <FontAwesome name="lock" size={44} color="black" />
-                <Text className='text-2xl font-semibold'>
-                    Trouble loggin in?
-                </Text>
+            </TouchableOpacity> */}
+                <View className='w-full flex items-center space-y-5'>
+                    <FontAwesome name="lock" size={44} color="black" />
+                    <Text className='text-2xl font-semibold'>
+                        Trouble logging in?
+                    </Text>
+                </View>
+                <View className='w-full flex flex-row justify-center my-5'>
+                    <Text className='text- '>
+                        Enter your email address and we'll send you a link to
+                        reset your password.
+                    </Text>
+                </View>
+                <TextInput
+                    placeholder='Enter email '
+                    className={`p-2 px-5 flex items-center border bg-gray-200 rounded-lg ${Platform.OS === 'ios' ? 'py-4' : 'py-2'} `}
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <TouchableOpacity
+                    onPress={sendResetLink}
+                    className='py-5 bg-black flex flex-row justify-center my-5 rounded-lg'>
+                    <Text className='text-white font-bold'>
+                        Send link
+                    </Text>
+                </TouchableOpacity>
             </View>
-            <View className='w-full flex flex-row justify-center my-5'>
-                <Text className='text- '>
-                    Enter your email address and we'll send you a link to
-                    reset your password.
-                </Text>
-            </View>
-            <TextInput
-                placeholder='Enter email '
-                className={`p-2 px-5 flex items-center border bg-gray-200 rounded-lg ${Platform.OS === 'ios' ? 'py-4' : 'py-2'} `}
-                value={email}
-                onChangeText={(text) => setEmail(text)}
-            />
-            <TouchableOpacity
-                onPress={sendResetLink}
-                className='py-5 bg-black flex flex-row justify-center my-5 rounded-lg'>
-                <Text className='text-white font-bold'>
-                    Send link
-                </Text>
-            </TouchableOpacity>
-        </View>
+        </>
     )
 }
 
