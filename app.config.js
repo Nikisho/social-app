82baad2a9cc26cea7dad7b0f53d228cbf5e5b747
Romain Phobere <phobereromain@gmail.com>
2026-05-21 22:34:38 +0100

    changed version

--- app.config.js
+++ app.config.js
@@ -49,7 +49,7 @@ export default {
     ],
     name: "Linkzy",
     slug: "social-app",
-    version: "1.3.17",
+    version: "1.3.18",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -63,7 +63,7 @@ export default {
       bundleIdentifier: 'com.linkzy',
       usesAppleSignIn: true,
       googleServicesFile: "./GoogleService-Info.plist",
-      buildNumber: '1.2.22',
+      buildNumber: '1.2.23',
       deploymentTarget: "12.0"
     },
     android: {
@@ -71,7 +71,7 @@ export default {
       // ndkVersion: "26.1.10909125",
       package: "com.linkzy",
       useNextNotificationsApi: true,
-      versionCode: 44,
+      versionCode: 45,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
