9973cd62d8f2b04905318641231fbe11ef68d491
Romain Phobere <phobereromain@gmail.com>
2026-07-22 14:13:09 +0100

    changed build number

--- app.config.js
+++ app.config.js
@@ -72,7 +72,7 @@ export default {
       // ndkVersion: "26.1.10909125",
       package: "com.linkzy",
       useNextNotificationsApi: true,
-      versionCode: 56,
+      versionCode: 57,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
